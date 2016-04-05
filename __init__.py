ce3db559f21ca96ae60178fe44c260bec3a121e5
Nicolas <nico.raynaud@gmail.com>
2016-04-05 18:26:09 +1100

    Update __init__.py
    Update precedence in import to allow install

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import tekinico_accounting_line
 import tekinico_accounting_category
-import tekinico_accounting_balance
 import tekinico_accounting_account
+import tekinico_accounting_balance
 import wizard
