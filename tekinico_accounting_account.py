3acdcce62a8ab59ab16e5d271a5fe32071c4e623
nicolas <tekinico@gmail.com>
2016-09-29 19:05:20 +1100

    changing future balance into future end of month and future end of month +1

--- tekinico_accounting_account.py
+++ tekinico_accounting_account.py
@@ -2,6 +2,9 @@
 from openerp.osv import fields, osv
 import logging
 import time
+from calendar import monthrange
+import datetime
+from dateutil import relativedelta
 
 _logger = logging.getLogger(__name__)
 
@@ -16,7 +19,7 @@ class tekinico_accounting_account(osv.osv):
 
         line_obj = self.pool.get('tekinico.accounting.line')
         for bid in ids:
-            lines = line_obj.search(cr, uid, [('account_id', '=', bid), '|', ('date_effect', '<=', time.strftime("%Y-%m-%d")), ('date_effect', '=', None)], order='date')
+            lines = line_obj.search(cr, uid, [('account_id', '=', bid), ('date', '<=', time.strftime("%Y-%m-%d"))], order='date')
             res[bid] = 0
             for line in line_obj.browse(cr, uid , lines, context=context):
                 res[bid] = res[bid] + (line.credit - line.debit)
@@ -36,12 +39,31 @@ class tekinico_accounting_account(osv.osv):
         return res
 
 
-    def _get_balance_future_id(self, cr, uid, ids, name, args, context):
+    def _get_balance_month_id(self, cr, uid, ids, name, args, context):
         res = {}
 
         line_obj = self.pool.get('tekinico.accounting.line')
+        date = datetime.datetime.now()
+        lastdayofmonth = monthrange(date.year, date.month)[1]
+        max_date = "%s-%s-%s" % (date.year, date.month, lastdayofmonth)
         for bid in ids:
-            lines = line_obj.search(cr, uid, [('account_id', '=', bid)], order='date')
+            lines = line_obj.search(cr, uid, [('account_id', '=', bid), ('date', '<=', max_date)], order='date')
+            res[bid] = 0
+            for line in line_obj.browse(cr, uid , lines, context=context):
+                res[bid] = res[bid] + (line.credit - line.debit)
+
+        return res
+
+    def _get_balance_next_month_id(self, cr, uid, ids, name, args, context):
+        res = {}
+
+        line_obj = self.pool.get('tekinico.accounting.line')
+
+        date = datetime.date.today() + relativedelta.relativedelta(months=1)
+        lastdayofmonth = monthrange(date.year, date.month)[1]
+        max_date = "%s-%s-%s" % (date.year, date.month, lastdayofmonth)
+        for bid in ids:
+            lines = line_obj.search(cr, uid, [('account_id', '=', bid), ('date', '<=', max_date)], order='date')
             res[bid] = 0
             for line in line_obj.browse(cr, uid , lines, context=context):
                 res[bid] = res[bid] + (line.credit - line.debit)
@@ -54,7 +76,8 @@ class tekinico_accounting_account(osv.osv):
         'type': fields.selection([('checking','Checking'),('savings','Savings')], 'Type'),
         'account_number': fields.char('Account Number'),
         'balance' : fields.function(_get_balance_id, type='integer', string='Balance'),
-        'balance_future' : fields.function(_get_balance_future_id, type='integer', string='Balance future'),
+        'balance_month' : fields.function(_get_balance_month_id, type='integer', string='Balance eom'),
+        'balance_next_month' : fields.function(_get_balance_next_month_id, type='integer', string='Balance eom+1'),
         'balance_ticked' : fields.function(_get_balance_ticked_id, type='integer', string='Balance ticked')
     }
 
